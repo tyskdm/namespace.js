f1276068a634694c62c1b20cac12453d1a673a89
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2012-12-17 00:47:02 +0900

    update namespace.js

--- src/namespace.js
+++ src/namespace.js
@@ -1,21 +1,79 @@
-/**
- * My next step of "Hello javascript world!"
- * Clone created on VAIO type P
- * 
+/** 
+ * My next step of "Hello javascript world."
+ * @fileOverview namespace function for Google apps script.
+ * @author <a href="mailto:tsuyoshi.kodama@gmail.com">Tsuyoshi Kodma</a> 
+ * @version 0.3.0
  */
 
+/**
+ * namespace library.</br>
+ * This static library includes create, define and require
+ * @namespace holds namespace controll methods.
+ */
+var namespace = (function (globalObject) {
+  var global_ = globalObject;
+  var definedNamespaces_ = [];
 
-var namespace = (function (global) {
+  /**
+   * create namespace.
+   * @param {string} nsString: name space
+   * @param {object} nsObject: object to set name space
+   * @returns {object} new namespace
+   */
+  function namespace(nsString, nsObject) {
+    var parent = global_,
+        parts = nsString.split('.'),
+        i, l;
+  
+    for (i = 0, l = parts.length; i < l; i++) {
+  
+      if (typeof parent[parts[i]] === "undefined") {
+        if (i < l-1) {
+          parent[parts[i]] = {};
+        } else {
+          parent[parts[i]] = nsObject || {};
+        }
+      }
+      parent = parent[parts[i]];
+    }
+  
+    return parent; // at last, parent is the leaf.
+  };
 
-  var namespace = function () {
-    return true;
+  /**
+   * define namespace with constractor.
+   * @param {string} namespace name
+   * @param {function} namespace constractor
+   */
+  function define(nsString, nsFunction) {
+    definedNamespaces_.push({
+      name: nsString,
+      func: nsFunction,
+      stat: false
+    });
   };
 
 
+  /**
+   * require namespace
+   * @param {string} requiredNamespace
+   * @returns {object} required object
+   */
+  function require(requiredNamespace) {
+    var i, l = definedNamespaces_.length();
+    
+    for (i=0; i<l; i++) {
+      if (definedNamespace_[i].name == requiredNamespace) {
+        definedNamespace_[i].stat == true;
+      }
+    }
+  };
+
+  var exports = namespace;
+  exports.define = define;
+  exports.require = require;
+  
   return exports;
 
-})(this);
-
-
-
+})(this); // 'this' is global object
 
