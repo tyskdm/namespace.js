7ad880d7d73dfef3aced248f2ae2d456e2a323ea
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2013-07-11 22:46:31 +0900

    backup

---
+++ ext/rajah.ext.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Externs for Rajah.
+ *
+ * @see http://pivotal.github.com/jasmine/
+ * @externs
+ */
+
+/** @const */
+var Rajah = {};
+
+/**
+ * @param {Object} globalObject to set Global object to rajah.
+ */
+Rajah.init = function(globalObject) {};
+
